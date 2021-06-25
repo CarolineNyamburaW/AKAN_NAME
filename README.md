a47323dc08834d20b60c80a2b71860f3466ba37f
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-06-25 17:50:09 +0300

    made corrections on the errors in my code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # {Caroline W. Nyambura}
-## {AKAN_NAMES}, {2021}
+## {AKAN NAME GENERATOR}, {2021}
 
 ## Description
 {This is a web application that takes a user's birthday and calculates the day of the week they were born and then depending on their gender outputs their Akan Name. Akan names are derived from Ghanian culture. Frequently in Ghana, children are given their first name as a 'day name' which corresponds to the day in the week they were born. Its a Website you can easily navigate though on you phone,tablet,laptop or desktop computer:No installation whatsoever required.}
@@ -18,7 +18,7 @@ https://carolinenyamburaw.github.io/AKAN_NAME/
 MIT License
 Copyright (c) [2021] [Caroline W. Nyambura]
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this website and associated documentation files (the "MY PORTFOLIO"), to deal
+of this website and associated documentation files (the "AKAN NAME GENERATOR"), to deal
 in the Website without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Website, and to permit persons to whom the Website is
--- static/js/main.js
+++ static/js/main.js
@@ -1,60 +1,38 @@
-function getUserInput() {
-  let inputArr = [];
-  let date = prompt("Enter Date You Were Born");
-  date = parseInt(date);
-  if (date < 1 || date > 31){
-    alert("Invalid Date");
-  } else {
-      inputArr.push = [date];
-      let month = prompt("Enter Month You Were Born");
-      month = parseInt(month);
-      if (month < 1 || month > 12){
-        alert("Invalid Month");
-     } else {
-      inputArr.push = [month];
-        let year = prompt("Enter Year You Were Born");
-        if (year.length !== 4){
-         alert("Invalid Year");
-        }else {
-          year = parseInt(year)
-          inputArr.push = [year];
-          return inputArr;
-        }
-     }
-  } return [0, 0, 0000]
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const maleNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function birthIndex(d, m, yr) {
+    let dd = parseInt(d);
+    let mm = parseInt(m);
+    let cc = Math.floor(yr / 100);
+    let yy = parseInt(yr.slice(2, 4));
+    let dayOfTheWeek = Math.floor((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7);
+    return dayOfTheWeek;
 }
 
-
-function calculateDayIndex(inputArr) {
-  let day = inputArr [0]
-  let month = inputArr [1]
-  let year = inputArr [2]
-  let day = 20
-  let index = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
-  return parseInt(index.toString().split(".")[0]);
-
-}
-
-function main() {
-  let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  let userInputArr = getUserInput();
-  if (userInputArr[0] === 0){
-    alert ("Input was Invalid.Try Again");
-    return [ "", ""];
-   } else {
-    let index = calculateDayIndex (userInputArr);
-    let day = daysOfTheWeek[index];
-    let name = "";
-    let gender = prompt ("Enter Your Gender");
-    if (gender.toLowerCase().startsWith("m")) {
-      name = maleNames[index];
-    } else {
-      name = femaleNames[index];
+function UserData() {
+    let day = document.getElementById("day").value;
+    let month = document.getElementById("month").value;
+    let year = document.getElementById("year").value;
+    if (day < 0 || day > 31 || month < 0 || month > 12) {
+        alert("Invalid day or month") & document.querySelector("form").reset()
     }
-    alert("You were born on " + day + "\nYour Akan name is " + name);
+    
+    let dayIndex = birthIndex(day, month, year);
+    return dayIndex;
+}
 
-   }
+function output(event) {
+    event.preventDefault();
+    let data = UserData();
+    if (document.getElementById("male").checked) {
+        alert("You were Born on "+ daysOfTheWeek[data] +"\nYour Akan Name is " + maleNames[data]);
+
+    } else if (document.getElementById("female").checked) {
+        alert("You were Born on "+ daysOfTheWeek[data] + "\nYour Akan Name is " + femaleNames[data]);
+    }
+    document.querySelector("form").reset()
 
 }
+document.getElementById("submit").addEventListener("click", output)
